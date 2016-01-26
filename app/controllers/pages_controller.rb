72ceba23293cdf58b38d4995ee89d1901bd2be3c
Jia Yee <jiayeerawr@gmail.com>
2016-01-26 14:24:01 +0800

    Google Login Attempt
    1. Not a smooth integration of Google API into Rails
    2. Security failure

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def login
+  end
+
+  def dashboard
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
+  resources :users
+  # get 'welcome/index'
+  
+  get '/login', to: 'pages#login', as: 'login'
+  get '/dashboard', to: 'pages#dashboard', as: 'dashboard'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
-
+  # root 'welcome#index'
+  root 'pages#login'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
